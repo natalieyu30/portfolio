bbc7f1c1fa3e84c44a265dced930a4dc693fd2d0
Natalie Yu <natalieyu30@gmail.com>
2021-06-28 18:45:46 -0400

    Add 'Midori Fresh website' to 'work page'

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,14 +5,19 @@ const menu = document.querySelector('.menu');
 const menuBranding = document.querySelector('.menu-branding');
 const menuNav = document.querySelector('.menu-nav');
 const navItems = document.querySelectorAll('.nav-item');
+const navLinks = document.querySelectorAll('.nav-link')
 
 
 // Set initial state of menu
 let showMenu = false;
 
 menuBtn.addEventListener('click', toggleMenu);
+navLinks.forEach(link => {
+  link.addEventListener('click', toggleMenu);
+})
 
 function toggleMenu() {
+  showMenu = (menuBtn.classList.contains('close'))? true : false;
   if (!showMenu) {
     menuBtn.classList.add('close');
     menu.classList.add('show');
@@ -35,6 +40,18 @@ function toggleMenu() {
   }
 }
 
+window.addEventListener('click', (e) => {
+  if(showMenu && !e.target.classList.contains('menu') && !e.target.classList.contains('menu-btn')) {
+    menuBtn.classList.remove('close');
+    menu.classList.remove('show');
+    menuBranding.classList.remove('show');
+    menuNav.classList.remove('show');
+    navItems.forEach(item => item.classList.remove('show'));
+
+    // set menu state
+    // showMenu= false;
+  }
+})
 
 
 
